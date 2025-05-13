d7df724b5ed2aabd8fc40f0ea1d3be0c341bb8e1
thqnam <141632310+thqnam@users.noreply.github.com>
2025-05-13 10:38:32 +0000

    14

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | QNED Dashboard',
+    default: 'QNED Dashboard',
+  },
+  description: 'The official Dashboard of QNED, built with App Router.',
+  metadataBase: new URL('https://qned.vercel.app/'),
+};
 
 export default function RootLayout({
   children,
