3b416bedafc5d8bdeb018c1b3d9f0445552409c7
thqnam <141632310+thqnam@users.noreply.github.com>
2025-05-14 11:05:16 +0000

    18

--- app/page.tsx
+++ app/page.tsx
@@ -23,9 +23,8 @@ export default function Page() {
             </a>
             , supporting by {' '}
             <a href="https://vercel.com/home" className="text-blue-500" target='_blank'>
-              Vercel 
+              Vercel firm.
             </a>
-            firm.
           </p>
           <Link
             href="/login"
