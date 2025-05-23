d283a94412df89c3697a51a78842c66612f63f6a
thqnam <141632310+thqnam@users.noreply.github.com>
2025-05-23 21:30:31 +0700

    54

--- auth.config.ts
+++ auth.config.ts
@@ -10,7 +10,7 @@ export const authConfig = {
         signIn: '/login',
     },
     callbacks: {
-    async authorized({ auth, request: { nextUrl } }) {
+    authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
       if (isOnDashboard) {
