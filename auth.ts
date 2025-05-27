148f225cf9c4412e968d2ad7b96634d89f9fdfde
thqnam <141632310+thqnam@users.noreply.github.com>
2025-05-27 13:28:18 +0700

    82

--- auth.ts
+++ auth.ts
@@ -5,16 +5,25 @@ import { z } from 'zod';
 import type { User } from '@/app/lib/definitions';
 import bcrypt from 'bcryptjs';
 import postgres from 'postgres';
+import { supabase } from '@/app/lib/supabaseClient';
  
-const sql = postgres(process.env.QNEDSPB_POSTGRES_URL!, { ssl: 'require' });
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
  
 async function getUser(email: string): Promise<User | undefined> {
   try {
-    const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
-    return user[0];
+    const { data, error } = await supabase
+      .from('users')
+      .select('*')
+      .eq('email', email)
+    if (error) {
+      throw error
+    } else {
+      const user = data as User[];
+      return user[0];
+    }
   } catch (error) {
-    console.error('Failed to fetch user:', error);
-    throw new Error('Failed to fetch user.');
+    console.error('Failed to fetch user: ', error);
+    throw new Error('Failed to fetch user.'); 
   }
 }
  
