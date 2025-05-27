341859a2b58e79d95f83d8ea4b29e314e8a332a2
thqnam <141632310+thqnam@users.noreply.github.com>
2025-05-27 14:55:03 +0700

    84

--- app/ui/UserGreeting.tsx
+++ app/ui/UserGreeting.tsx
@@ -4,8 +4,13 @@ export default async function UserGreeting() {
   const session = await auth();
   if (!session?.user) return null;
   return (
-    <div className="mb-2 text-sm text-gray-700">
-      👋 Welcome, <b>{session.user.name || session.user.email}</b>
+    <div className='flex justify-between'>
+        <div className="mb-2 text-sm text-gray-700">
+            👋 Welcome, <b>{session.user.name || session.user.email}</b>
+        </div>
+        <div className="mb-2 text-sm text-gray-700">
+            Email: <b>{session.user.email}</b>
+        </div>
     </div>
   );
 }
