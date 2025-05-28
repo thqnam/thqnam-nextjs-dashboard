d16497f6ab1b541f3d1dba8b05fa4b41bb1d33a8
thqnam <141632310+thqnam@users.noreply.github.com>
2025-05-28 14:01:14 +0700

    93

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -457,8 +457,8 @@ export async function authenticate(
   formData: FormData,
 ) {
   try {
-    await signIn('credentials', formData);
     await changeUserStatusLogin();
+    await signIn('credentials', formData);
   } catch (error) {
     if (error instanceof AuthError) {
       switch (error.type) {
