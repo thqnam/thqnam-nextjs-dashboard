d70a57ade5dc1ce34a3c7ec388e01ef10fbe8b5e
thqnam <141632310+thqnam@users.noreply.github.com>
2025-05-28 13:46:30 +0700

    91

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -4,10 +4,8 @@ import NavLinks from '@/app/ui/dashboard/nav-links';
 import AcmeLogo from '@/app/ui/acme-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
 import { resetTarget, logOut } from '@/app/lib/actions';
-import { useActionState } from 'react';
 
 export default function SideNav() {
-  const [state, formAction, isPending] = useActionState(logOut, undefined);
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <button
@@ -22,12 +20,9 @@ export default function SideNav() {
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
         <form
-          action={() => {formAction}}
+          action={() => {logOut()}}
         >
-          <button 
-            className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
-            aria-disabled={isPending}
-          >
+          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
             <PowerIcon className="w-6" />
             <div className="hidden md:block">Sign Out</div>
           </button>
