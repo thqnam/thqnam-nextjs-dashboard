71eb21bdb67ecf8a7adec304b2fcb4b198e2669b
thqnam <141632310+thqnam@users.noreply.github.com>
2025-05-30 20:37:08 +0700

    126

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -47,9 +47,6 @@ export default async function Page(props: {
         ]}
       />
       <div className="w-full">
-        <div className="flex w-full items-center justify-between">
-          <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
-        </div>
         <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
           <Search placeholder="Search customers..." />
           <CreateCustomer />
