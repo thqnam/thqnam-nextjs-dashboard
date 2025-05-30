7e45f023fd384512c0263ff18d1b21e95c14c3fd
thqnam <141632310+thqnam@users.noreply.github.com>
2025-05-30 17:42:17 +0700

    122

--- app/page.tsx
+++ app/page.tsx
@@ -33,16 +33,16 @@ export default function Page() {
             {' '} firm.
           </p>
           <Link
-            href="/signup"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
-            <span>Sign Up</span> <ArrowUpIcon className="w-5 md:w-6" />
-          </Link>
-          <Link
             href="/signin"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
             <span>Sign In</span> <ArrowRightIcon className="w-5 md:w-6" />
+          </Link>
+          <Link
+            href="/signup"
+            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+          >
+            <span>Sign Up</span> <ArrowUpIcon className="w-5 md:w-6" />
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
