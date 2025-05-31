ae86e84b1f0e452b8837cb6a7029c22be08bf586
thqnam <141632310+thqnam@users.noreply.github.com>
2025-05-31 15:55:06 +0700

    134

--- app/ui/customers/edit-form.tsx
+++ app/ui/customers/edit-form.tsx
@@ -183,6 +183,7 @@ export default function EditCustomerForm({ id }: { id: string }) {
                 name="image_url"
                 className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
                 value={selectedImage}
+                onChange={e => setSelectedImage(e.target.value)}
                 aria-describedby="image_url-error"
                 required
                 autoFocus
@@ -225,7 +226,6 @@ export default function EditCustomerForm({ id }: { id: string }) {
                 defaultValue={customer.name}
                 aria-describedby="name-error"
                 required
-                autoFocus
               />
               <InformationCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
