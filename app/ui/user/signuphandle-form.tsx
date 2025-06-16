eb0f3e7fcc5ab0277292f4de4f0a3063af6b0703
thqnam <141632310+thqnam@users.noreply.github.com>
2025-06-16 17:24:06 +0700

    299

--- app/ui/user/signuphandle-form.tsx
+++ app/ui/user/signuphandle-form.tsx
@@ -211,17 +211,17 @@ export default function Form( {email, name, image} : FormProps) {
         </Link>
         <Link 
           className="flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 mt-4 w-full" 
-          href="/signuprequest"
-          aria-disabled={isPending}
-        >
-          Sign Up Request <ArrowUpIcon className="ml-auto h-5 w-5 text-gray-50" />
-        </Link>
-        <Link 
-          className="flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 mt-4 w-full" 
           href="/resetpassrequest"
           aria-disabled={isPending}
         >
           Reset Pass Request <ArrowTurnDownRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+        </Link>
+        <Link 
+          className="flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 mt-4 w-full" 
+          href="/signuprequest"
+          aria-disabled={isPending}
+        >
+          Sign Up Request <ArrowUpIcon className="ml-auto h-5 w-5 text-gray-50" />
         </Link>
         <Link 
           className="flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 mt-4 w-full" 
