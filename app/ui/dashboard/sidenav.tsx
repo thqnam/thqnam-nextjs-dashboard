bc718f17fe3069244de601621ab92a9980b3fb75
thqnam <141632310+thqnam@users.noreply.github.com>
2025-09-11 21:16:39 +0700

    345

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -61,7 +61,7 @@ export default function SideNav() {
     return () => {
       supabase.removeChannel(channel);
     };
-  }, []);
+  }, [sessionId]);
   
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
