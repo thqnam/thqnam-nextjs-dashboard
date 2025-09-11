e3f2282a2909cbfb261b53a21fe5257ba027d100
thqnam <141632310+thqnam@users.noreply.github.com>
2025-09-11 19:47:54 +0700

    338

--- app/ui/user/changeinfor-form.tsx
+++ app/ui/user/changeinfor-form.tsx
@@ -178,7 +178,7 @@ export default function Form() {
                   id="image"
                   name="image"
                   className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-                  value={selectedImage}
+                  defaultValue={user.image}
                   onChange={e => setSelectedImage(e.target.value)}
                   aria-describedby="image-error"
                   required
@@ -219,7 +219,7 @@ export default function Form() {
                   name="name"
                   placeholder="Enter your nick name"
                   aria-describedby='name-error'
-                  value={user.name}
+                  defaultValue={user.name}
                   required
                 />
                 <InformationCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
