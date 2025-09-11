d6a283fdf49dcc8527985976bf5f4782cdfae2df
thqnam <141632310+thqnam@users.noreply.github.com>
2025-09-11 20:06:01 +0700

    341

--- app/ui/user/changeinfor-form.tsx
+++ app/ui/user/changeinfor-form.tsx
@@ -219,7 +219,7 @@ export default function Form() {
                   name="name"
                   placeholder="Enter your nick name"
                   aria-describedby='name-error'
-                  value={sessionName}
+                  defaultValue={sessionName}
                   required
                 />
                 <InformationCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
