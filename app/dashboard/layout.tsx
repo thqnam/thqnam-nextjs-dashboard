a56e79aba96bab68a136efa6fd1771a051a79c06
thqnam <141632310+thqnam@users.noreply.github.com>
2025-09-11 17:54:55 +0700

    331

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,5 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
 import { SessionInforProvider } from '@/app/lib/sessionInforContext';
-import { Suspense } from 'react';
 
 export const experimental_ppr = true;
  
@@ -9,9 +8,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <SessionInforProvider>
       <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
         <div className="w-full flex-none md:w-64">
-          <Suspense>
-            <SideNav />
-          </Suspense>
+          <SideNav />
         </div>
         <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
       </div>
