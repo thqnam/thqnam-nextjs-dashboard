bd6afc98bc6d4f546d70f84c23694ba4f4e27b25
thqnam <141632310+thqnam@users.noreply.github.com>
2025-10-01 17:58:01 +0700

    354

--- app/ui/sidelink.tsx
+++ app/ui/sidelink.tsx
@@ -8,37 +8,37 @@ export default function SideLink() {
   return (
     <>
         <Link
-            href="/signinrequest"
+            href="/users/signinrequest"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
         >
             <span>Sign In Request</span> <ArrowRightIcon className="w-5 md:w-6" />
         </Link>
         <Link
-            href="/resetpassrequest"
+            href="/users/resetpassrequest"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
         >
             <span>Reset Pass Request</span> <ArrowTurnDownRightIcon className="w-5 md:w-6" />
         </Link>
         <Link
-            href="/signuprequest"
+            href="/users/signuprequest"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
         >
             <span>Sign Up Request</span> <ArrowUpIcon className="w-5 md:w-6" />
         </Link>
         <Link
-            href="/changemailfromrequest"
+            href="/users/changemailfromrequest"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
         >
             <span>Change Mail From Request</span> <ArrowTurnDownLeftIcon className="w-5 md:w-6" />
         </Link>
         <Link
-            href="/resetemailrequest"
+            href="/users/resetemailrequest"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
         >
             <span>Reset Email Request</span> <ArrowTurnDownLeftIcon className="w-5 md:w-6" />
         </Link>
         <Link
-            href="/signdownrequest"
+            href="/users/signdownrequest"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
         >
             <span>Sign Down Request</span> <ArrowDownIcon className="w-5 md:w-6" />
