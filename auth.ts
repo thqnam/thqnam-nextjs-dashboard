3cde31b7110c1c9e9c7a8de9d06a7a0d455083ba
thqnam <141632310+thqnam@users.noreply.github.com>
2026-08-20 21:49:46 +0700

    372

--- auth.ts
+++ auth.ts
@@ -16,7 +16,7 @@ export const { auth, signIn, signOut, unstable_update } = NextAuth({
     Credentials({
       async authorize(credentials) {
         const parsedCredentials = z
-          .object({ email: z.string().email(), password: z.string() })
+          .object({ email: z.email(), password: z.string() })
           .safeParse(credentials);
 
         if (!parsedCredentials.success) return null;
