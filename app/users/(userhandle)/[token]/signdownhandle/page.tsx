2f9149b9f98411ee31a4b1016fbaf1ee0589f7f1
thqnam <141632310+thqnam@users.noreply.github.com>
2026-09-03 22:26:20 +0700

    398

--- app/users/(userhandle)/[token]/signdownhandle/page.tsx
+++ app/users/(userhandle)/[token]/signdownhandle/page.tsx
@@ -5,57 +5,11 @@ import { Metadata } from 'next';
 import { getUserByToken } from '@/app/lib/utils';
 import { deleteDatabaseToken } from '@/app/lib/actions/functions/users';
 import { notFound } from 'next/navigation';
+import { standardMetadata } from '@/app/lib/standardMetadata';
  
 export const metadata: Metadata = {
   title: 'Reset Pass Handle',
-  icons: `${process.env.APP_ICON}`,
-  applicationName: `${process.env.APP_NAME}`,
-  description: `The official Web Page of ${process.env.APP_NAME} App, built by Mr. ${process.env.APP_OWNER}.`,
-  authors: [{name: `${process.env.APP_OWNER}`, url: `${process.env.OWNER_INFOR}`}],
-  assets: `${process.env.APP_ASSET}`,
-  archives: `${process.env.APP_ARCHIVES}`,
-  creator: `${process.env.APP_OWNER}`,
-  generator: `${process.env.APP_GENERATOR}`,
-  keywords: `${process.env.APP_NAME}, ${process.env.APP_NAME} App, ${process.env.APP_OWNER}`,
-  publisher: `${process.env.APP_PUBLISHER}`,
-  classification: "Business",
-  category: "Technology",
-  openGraph: {
-    type: 'website',
-    url: `${process.env.APP_HOMEPAGE}`,
-    title: `${process.env.APP_NAME} App`,
-    description: `The official Web Page of ${process.env.APP_NAME} App, built by Mr. ${process.env.APP_OWNER}.`,
-    siteName: `${process.env.APP_NAME}`,
-    locale: "vi_VN",
-    alternateLocale: ["en_US"],
-    emails: ["thieuhuynhquangnam1996@gmail.com"],
-    countryName: "Vietnam",
-    phoneNumbers: ["+84937821788"],
-    images: [
-      { 
-        url: `${process.env.APP_ICON}`, 
-        alt: `${process.env.APP_NAME}'s picture`,
-        type: "image/png",
-        secureUrl: `${process.env.APP_ICON}`,
-      }
-    ],
-  },
-  twitter: {
-    card: 'summary_large_image',
-    site: `${process.env.APP_HOMEPAGE}`,
-    creator: `${process.env.APP_OWNER}`,
-    description: `The official Web Page of ${process.env.APP_NAME} App, built by Mr. ${process.env.APP_OWNER}.`,
-    title: `${process.env.APP_NAME} App`,
-    creatorId: "@NamThieu1996",
-    images: [
-      { 
-        url: `${process.env.APP_ICON}`, 
-        alt: `${process.env.APP_NAME}'s picture`,
-        type: "image/png",
-        secureUrl: `${process.env.APP_ICON}`,
-      }
-    ],
-  }
+  ...standardMetadata,
 };
  
 export default async function Page(props: { params: Promise<{ token: string }> }) {
--- app/users/(userrequest)/[token]/changemailtorequest/not-found.tsx
+++ app/users/(userrequest)/[token]/changemailtorequest/not-found.tsx
@@ -1,57 +1,11 @@
 import Link from 'next/link';
 import { FaceFrownIcon } from '@heroicons/react/24/outline';
 import { Metadata } from 'next';
+import { standardMetadata } from '@/app/lib/standardMetadata';
  
 export const metadata: Metadata = {
   title: 'Not-Found Token Change Email From Request',
-  icons: `${process.env.APP_ICON}`,
-  applicationName: `${process.env.APP_NAME}`,
-  description: `The official Web Page of ${process.env.APP_NAME} App, built by Mr. ${process.env.APP_OWNER}.`,
-  authors: [{name: `${process.env.APP_OWNER}`, url: `${process.env.OWNER_INFOR}`}],
-  assets: `${process.env.APP_ASSET}`,
-  archives: `${process.env.APP_ARCHIVES}`,
-  creator: `${process.env.APP_OWNER}`,
-  generator: `${process.env.APP_GENERATOR}`,
-  keywords: `${process.env.APP_NAME}, ${process.env.APP_NAME} App, ${process.env.APP_OWNER}`,
-  publisher: `${process.env.APP_PUBLISHER}`,
-  classification: "Business",
-  category: "Technology",
-  openGraph: {
-    type: 'website',
-    url: `${process.env.APP_HOMEPAGE}`,
-    title: `${process.env.APP_NAME} App`,
-    description: `The official Web Page of ${process.env.APP_NAME} App, built by Mr. ${process.env.APP_OWNER}.`,
-    siteName: `${process.env.APP_NAME}`,
-    locale: "vi_VN",
-    alternateLocale: ["en_US"],
-    emails: ["thieuhuynhquangnam1996@gmail.com"],
-    countryName: "Vietnam",
-    phoneNumbers: ["+84937821788"],
-    images: [
-      { 
-        url: `${process.env.APP_ICON}`, 
-        alt: `${process.env.APP_NAME}'s picture`,
-        type: "image/png",
-        secureUrl: `${process.env.APP_ICON}`,
-      }
-    ],
-  },
-  twitter: {
-    card: 'summary_large_image',
-    site: `${process.env.APP_HOMEPAGE}`,
-    creator: `${process.env.APP_OWNER}`,
-    description: `The official Web Page of ${process.env.APP_NAME} App, built by Mr. ${process.env.APP_OWNER}.`,
-    title: `${process.env.APP_NAME} App`,
-    creatorId: "@NamThieu1996",
-    images: [
-      { 
-        url: `${process.env.APP_ICON}`, 
-        alt: `${process.env.APP_NAME}'s picture`,
-        type: "image/png",
-        secureUrl: `${process.env.APP_ICON}`,
-      }
-    ],
-  }
+  ...standardMetadata,
 };
  
 export default function NotFound() {
